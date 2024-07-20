c4d53b2651453dd45136b8d4c9563e1d96d09b11
allan santos <allankelvin.santos@hotmail.com>
2024-07-20 16:52:34 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    MenuBarComponent,
+    CardComponent,
+    HomeComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Interface-PlayStation-Store';
+}
